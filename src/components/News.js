2fc9ed1809b92511dccaddcda794a3c75d286c29
mit-anuj <anujmittal0040@gmail.com>
2023-04-11 20:01:39 +0530

    code not working properly

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
+import Spinner from './Spinner';
 
 export class News extends Component {
 
@@ -7,40 +8,55 @@ export class News extends Component {
         super();
         this.state = {
             article: [],
-            loading: false,
+            loading: true,
             page: 1,
-            totalResult: 1
+            totalResult: 1,
+            error: null
         }
-        
+
     }
-    
+
     async componentDidMount() {
         try {
-            let url = 'https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=9225e6d8b8c04de6b9b4d0aaf3d213cc&page=1'
+            if (this.state.loading === true) {
+                <Spinner />
+            }
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business
+            &apiKey=9225e6d8b8c04de6b9b4d0aaf3d213cc&page=1&pageSize=${this.props.pageSize}`
             let data = await fetch(url);
+            // if(data.status >= 400  && data.status <= 600) throw data;
             let parsedData = await data.json()
             this.setState({
                 article: parsedData.articles,
-                totalResult: parsedData.totalResults
+                totalResult: parsedData.totalResults,
+                loading: false
             })
         }
         catch (e) {
-            console.log("something went wrong")
+            console.log(e)
+            // if(e instanceof Error) {
+            // this.setState({...this.state, error:e.message})
         }
     }
-// handles the next button.
+
+    // handles the next button.
     handleNextCase = async () => {
         try {
-            if (this.state.page + 1 > Math.ceil(this.state.totalResult / 20)) {
+            if (this.state.loading === true) {
+                <Spinner />
+            }
+            if (this.state.page + 1 > Math.ceil(this.state.totalResult / this.props.pageSize)) {
             } else {
                 //! here we are adding 1 to the page so that we can jump to the next page.
-                let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=9225e6d8b8c04de6b9b4d0aaf3d213cc&page=${this.state.page + 1}&pageSize=20`
+                let url = `https://newsapi.org/v2/top-headlines?country=in&category=business
+                &apiKey=9225e6d8b8c04de6b9b4d0aaf3d213cc&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
                 let data = await fetch(url);
                 let parsedData = await data.json()
                 this.setState({
                     article: parsedData.articles,
                     //! here this will update the value of page 
-                    page: this.state.page + 1
+                    page: this.state.page + 1,
+                    loading: false
                 })
             }
         }
@@ -50,15 +66,20 @@ export class News extends Component {
     }
     // handles the previous button.
     handlePrevCase = async () => {
+        if (this.state.loading === true) {
+            <Spinner />
+        }
         try {
             //! here we are adding 1 to the page so that we can jump to the next page.
-            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=9225e6d8b8c04de6b9b4d0aaf3d213cc&page=${this.state.page - 1}&pageSize=20`
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business
+            &apiKey=9225e6d8b8c04de6b9b4d0aaf3d213cc&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
             let data = await fetch(url);
             let parsedData = await data.json()
             this.setState({
                 article: parsedData.articles,
                 //! here this will update the value of page 
-                page: this.state.page - 1
+                page: this.state.page - 1,
+                loading: false
             })
         }
         catch (e) {
@@ -69,27 +90,33 @@ export class News extends Component {
         return (
             <div className='container'>
                 <h2>NewsMonkey - Top News</h2>
+                <Spinner />
                 <div className="row">
-                    
+
                     {/* using map function of array collection to iterate the array and fetch all the data from it.
-                        a function must be passed as a parameter to the map funtion so we created an arrow function. */}
-                    {this.state.article.map((element) => {
-                        return (<div className="col-md-4" key={element.url}>
-                            <p>this is a paragraph</p>
-                            {/* using slice method to restrict the number of characters on the card */}
-                            <Newsitem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
-                        </div>
-                        )
-                    })
+                        a function must be passed as a parameter to the map funtion so we created an arrow function. */
+                    }
+
+                    {
+
+                        this.state.error && this.state.article.map((element) => {
+                            return (<div className="col-md-4" key={element.url}>
+                                {/* using slice method to restrict the number of characters on the card */}
+                                <Newsitem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            </div>
+                            )
+                        })
                     }
                 </div>
                 <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevCase}>&larr; Previous</button>
-                    <button type="button" className="btn btn-dark" onClick={this.handleNextCase}>Next &rarr;</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResult / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextCase}>Next &rarr;</button>
+                    {/* {JSON.stringify(this.state.error)} */}
                 </div>
             </div>
         )
     }
 }
+
 
 export default News
